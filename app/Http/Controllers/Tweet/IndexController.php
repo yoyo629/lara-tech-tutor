678047a831f53d04f9f1f02a9f3af395c959771e
Yosuke <king.crimson.723.yu@gmail.com>
2025-02-03 21:24:23 +0900

    add:index.blade.php

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class IndexController extends Controller
 {
@@ -12,6 +13,13 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return 'Single Action';
+        // return view('tweet.index', ['name' => 'laravel']);
+
+        //Facade呼び出し
+        return View::make('tweet.index',['name' => 'laravel']);
+
+        //with関数
+        // return view('tweet.index')
+        //         ->with('name', 'laravel');
     }
 }
