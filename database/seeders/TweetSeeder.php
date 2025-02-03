1128141815bf0538539b276c497ca11bf6d7d23c
Yosuke <king.crimson.723.yu@gmail.com>
2025-02-03 21:56:00 +0900

    add:make TweetFactory

--- database/seeders/TweetSeeder.php
+++ database/seeders/TweetSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tweet;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,10 +16,6 @@ class TweetSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('tweets')->insert([
-            'content' => Str::random(100),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Tweet::factory()->count(10)->create();
     }
 }
