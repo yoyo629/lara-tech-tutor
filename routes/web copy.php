fef288af080f9c7c65bf9d4553c9623282092d4a
Yosuke <king.crimson.723.yu@gmail.com>
2025-02-05 23:48:21 +0900

    add:ログイン機能作成

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +20,7 @@ Route::get('/', function () {
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
 
-// Tweet
+// シングルアクション
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
 
@@ -33,14 +32,4 @@ Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutCont
 // 削除処理
 Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-require __DIR__.'/auth.php';
