a3bbc287c067c7d39b987508a1c1f00ef599ff24
Yosuke <king.crimson.723.yu@gmail.com>
2025-02-06 23:51:37 +0900

    del:Vite関連ファイルの削除

--- app/Http/Controllers/Tweet/DeleteController.php
+++ app/Http/Controllers/Tweet/DeleteController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers\Tweet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Tweet;
+use App\Services\TweetService;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class DeleteController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         $tweetId = (int) $request->route('tweetId');
+
+        if(!$tweetService->checkOwnTweet($request->user()->id, $tweetId)) {
+            throw new AccessDeniedHttpException();
+        }
 
         $tweet = Tweet::where('id', $tweetId)->firstOrFail();
 
--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tweet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use App\Services\TweetService;
 use App\Models\Tweet;
 
 class IndexController extends Controller
@@ -12,10 +13,10 @@ class IndexController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         //全つぶやき取得
-        $tweets = Tweet::orderby('created_at', 'DESC')->get();
+        $tweets = $tweetService->getTweets();
         return view('tweet.index')
                 ->with('tweets', $tweets);
     }
