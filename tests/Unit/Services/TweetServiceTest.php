53d4257d140f3f6359f3ede89da6019cb5fd2216
Yosuke <king.crimson.723.yu@gmail.com>
2025-02-11 18:56:49 +0900

    fix:オーナーチェックのUnitテスト

--- tests/Unit/Services/TweetServiceTest.php
+++ tests/Unit/Services/TweetServiceTest.php
@@ -2,15 +2,30 @@
 
 namespace Tests\Unit\Services;
 
+use Mockery;
+
 use PHPUnit\Framework\TestCase;
+use App\Services\TweetService;
 
 class TweetServiceTest extends TestCase
 {
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    public function test_check_own_tweet()
     {
-        $this->assertTrue(true);
+        $tweetService = new TweetService(); //インスタンス作成
+
+        $mock = Mockery::mock('alias:App\Models\Tweet');
+        $mock->shouldReceive('where->first')->andReturn((object)[
+            'id' => 1,
+            'user_id' => 1
+        ]);
+
+        $result = $tweetService->checkOwnTweet(1,1);
+        $this->assertTrue($result);
+
+        $result = $tweetService->checkOwnTweet(2,1);
+        $this->assertFalse($result);
     }
 }
