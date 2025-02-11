a510722a9564657237dfd854ac4ca5083657b53e
Yosuke <king.crimson.723.yu@gmail.com>
2025-02-11 19:12:39 +0900

    add:つぶやき削除テスト実装

--- tests/Feature/Tweet/DeleteTest.php
+++ tests/Feature/Tweet/DeleteTest.php
@@ -5,16 +5,30 @@ namespace Tests\Feature\Tweet;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Tweet;
 
 class DeleteTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_delete_successed()
     {
-        $response = $this->get('/');
+        // 削除用のユーザー作成
+        $user = User::factory()->create();
 
-        $response->assertStatus(200);
+        // 削除用の投稿作成
+        $tweet = Tweet::factory()->create(['user_id' => $user->id]);
+
+        // 指定ユーザーをログイン状態にする
+        $this->actingAs($user);
+
+        $response = $this->delete('/tweet/delete/' . $tweet->id); //作成した投稿のID指定
+
+        // 削除後のリダイレクト確認
+        $response->assertRedirect('/tweet');
     }
 }
